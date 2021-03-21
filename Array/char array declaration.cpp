b79b51bc24e15d5698159b21a92451de9601f0ef
Abhishek Kumar <60790645+Abhi23raj472@users.noreply.github.com>
2021-03-21 17:21:29 +0530

    Add files via upload

---
+++ Array/char array declaration.cpp
@@ -0,0 +1,16 @@
+ #include<iostream>
+using namespace std;
+int main(){
+	char arr[100]="Apple";
+	int i=0;
+	while(arr[i]!='\0'){
+		cout<<arr[i]<<" ";
+		i++;
+	}
+	return 0;/*
+	char arr[100];
+	cin>>arr;
+	cout<<arr[2];
+	return 0;*/
+	
+}
